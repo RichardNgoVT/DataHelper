43523055af3babfbd081792a7ee9d0a14d82bd36
Richard Ngo <R1ch4rd@vt.edu>
2020-12-28 14:01:07 -0500

    add clusterFind.py

---
+++ clusterFinder.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 13:49:32 2020
+
+"""
+#grouping functions
+#group by name
+
+
+
+
+#group by address
+
+
+
+
+#group by proximity
+
+
+
+
+#pairing functions (if spa and cont grouped seperately)
+#
+
